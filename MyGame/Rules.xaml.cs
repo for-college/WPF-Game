e5bd0d26144e47d449cd9ca25d3932fb9161bd73
scffs <67464545+scffs@users.noreply.github.com>
2022-11-08 13:41:23 +0300

    added functional

--- MyGame/Rules.xaml.cs
+++ MyGame/Rules.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,24 +23,43 @@ namespace MyGame
         public Rules()
         {
             InitializeComponent();
+            ruleButtonBefore.IsEnabled = false;
         }
         int counter;
-        private void RulesButton(object sender, RoutedEventArgs e)
-        {
-            
-            string[] rules = {"Используются слова только на русском языке", 
-                "Сквернословия не использутся", 
+        string[] rules = {"Используются слова только на русском языке",
+                "Сквернословия не использутся",
                 "Первую букву слова слова следует вводить в верхнем регистре","Удачной игры!"};
-            for(int i = 0; i < rules.Length; i++)
+        private void NextRule(object sender, RoutedEventArgs e)
+        {
+            if (counter == 1)
             {
-                rulesBlock.Text = rules[counter];
-                if(counter + 1 == rules.Length)
-                {
-                    ruleButton.IsEnabled = false;
-                }
-                
+                ruleButtonBefore.IsEnabled = true;
             }
+            if (counter + 1 == rules.Length)
+            {
+                ruleButtonNext.IsEnabled = false;
+            }
+            else
+            {
+                ruleButtonNext.IsEnabled = true;
+            }
+            rulesBlock.Text = $" [{counter + 1}/ 4] " + rules[counter];
             counter++;
+        }
+        private void PrevRule(object sender, RoutedEventArgs e)
+        {
+            counter--;
+            if (counter - 1 <= 0)
+            {
+                ruleButtonBefore.IsEnabled = false;
+                ruleButtonNext.IsEnabled = true;
+            }
+            else
+            {
+                ruleButtonBefore.IsEnabled = true;
+                ruleButtonNext.IsEnabled = true;
+            }
+            rulesBlock.Text = $" [{counter}/ 4] " + rules[counter - 1];
         }
     }
 }
