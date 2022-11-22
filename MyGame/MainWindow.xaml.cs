5ab7b071e5caf8e4e5c3748f9a26b43f0bbdc0f0
scffs <67464545+scffs@users.noreply.github.com>
2022-11-22 11:28:02 +0700

    Проработаны исключения

--- MyGame/MainWindow.xaml.cs
+++ MyGame/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,6 +22,7 @@ namespace MyGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        int roundsInt = Data.rounds;
         public MainWindow()
         {
             InitializeComponent();
@@ -41,22 +43,24 @@ namespace MyGame
 
         private void OpenGameList(object sender, RoutedEventArgs e)
         {
-            if (rounds.Text == "") Data.rounds = 5;
+            if (rounds.Text == "") roundsInt = 5;
             try
             {
-                if(rounds.Text != "") if (Convert.ToInt32(rounds.Text) > 0) Data.rounds = Convert.ToInt32(rounds.Text);
-                if (player1.Text != "" || player2.Text != "" && Convert.ToInt32(rounds.Text) > 0) GameList();
-                else MessageBox.Show("Заполните обязательные поля!", "Ошибочка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                if(rounds.Text != "") roundsInt = Convert.ToInt32(rounds.Text);
+                if(player1.Text == "" || player2.Text == "") throw new Exception("Не все обязательные поля заполнены");
+                if(roundsInt <= 0) throw new Exception("Количество раундов < 0");
+                else GameList();
             }
-            catch
-            {
-                MessageBox.Show("А количество раундов точно верно задано?", "Ошибочка", MessageBoxButton.OK, MessageBoxImage.Warning);
+            catch(Exception ex)
+            { 
+                MessageBox.Show(ex.Message, "Ошибочка", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
         }
         private void GameList()
         {
             Data.playerOne = player1.Text;
             Data.playerTwo = player2.Text;
+            Data.rounds = roundsInt;
             if (checkName1.IsChecked == false) player1.Text = "";
             if (checkName2.IsChecked == false) player2.Text = "";
             ChooseGame cg = new ChooseGame();
